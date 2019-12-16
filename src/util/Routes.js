7193050576e2a71a5e3b0384f3b44809b95ed1f1
George Bentes <george.bentes@gmail.com>
2019-12-15 23:07:02 -0300

    Add route param

--- src/util/Routes.js
+++ src/util/Routes.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-import Home from '../views/Home';
-import Pay from '../views/Pay';
-import Paid from '../views/Paid';
-import Report from '../views/Report';
-import Expense from '../views/Expense';
-import NotFound from '../views/NotFound';
+import Home from "../views/Home";
+import Pay from "../views/Pay";
+import Paid from "../views/Paid";
+import Report from "../views/Report";
+import Expense from "../views/Expense";
+import NotFound from "../views/NotFound";
 
-export default function Routes(){
-  return(
+export default function Routes() {
+  return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/pay" component={Pay} />
       <Route exact path="/paid" component={Paid} />
-      <Route exact path="/expense" component={Expense} />
+      <Route exact path="/expense/:id?" component={Expense} />
       <Route exact path="/report" component={Report} />
       <Route path="*" component={NotFound} />
     </Switch>
-  )
+  );
 }
