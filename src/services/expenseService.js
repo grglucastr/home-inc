f11ab6beb984996686b5538968d11a1b8594ed23
George Bentes <george.bentes@gmail.com>
2019-12-15 23:06:18 -0300

    Refactor service and add new method to find by id

--- src/services/expenseService.js
+++ src/services/expenseService.js
@@ -1,24 +1,31 @@
 import axios from "axios";
-import { BASE_URL } from "./constants";
+import { ROOT_URL } from "./constants";
+
+const resource = "/expenses";
+const BASE_URL = `${ROOT_URL}/${resource}`;
 
 export default {
   create: async expense => {
-    const url = `${BASE_URL}/expenses`;
-    const response = await axios.post(url, expense);
+    const response = await axios.post(BASE_URL, expense);
     const data = await response.data;
-
     return data;
   },
 
   update: async expense => {
-    const url = `${BASE_URL}/expenses/${expense.id}`;
+    const url = `${BASE_URL}/${expense.id}`;
     const response = await axios.put(url, expense);
     const data = await response.data;
     return data;
   },
 
   listAll: async _ => {
-    const url = `${BASE_URL}/expenses`;
+    const response = await axios.get(BASE_URL);
+    const data = await response.data;
+    return data;
+  },
+
+  findById: async id => {
+    const url = `${BASE_URL}/${id}`;
     const response = await axios.get(url);
     const data = await response.data;
     return data;
