1bf5d71600c793886784a730be6d050e472d9f47
George Bentes <george.bentes@gmail.com>
2020-01-10 22:06:51 -0300

    Add react switch and refactor filter panel

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import { Alert, Form, Button, Row, Col } from "react-bootstrap";
+import Switch from "react-switch";
+import DatePicker from "react-datepicker";
 
 const currentYear = new Date().getFullYear();
 const currentMonth = new Date().getMonth() + 1;
@@ -14,68 +17,96 @@ const emptyFilterParams = {
 
 export default function Filter({ onFilter }) {
   const [filterParams, setFilterParams] = React.useState(emptyFilterParams);
+  const [dateStart, setDateStart] = useState(new Date());
+  const [dateEnd, setDateEnd] = useState(new Date());
 
   return (
     <div>
-      <div>
-        <label>Due Date:</label>
-        <input
-          type="date"
-          name="start"
-          id="start"
-          value={filterParams.period.start}
-          onChange={evt =>
-            setFilterParams({
-              ...filterParams,
-              period: {
-                ...filterParams.period,
-                start: evt.target.value
-              }
-            })
-          }
-        />
-        <span> ~ </span>
-        <input
-          type="date"
-          name="end"
-          id="end"
-          value={filterParams.period.end}
-          onChange={evt =>
-            setFilterParams({
-              ...filterParams,
-              period: {
-                ...filterParams.period,
-                end: evt.target.value
-              }
-            })
-          }
-        />
-      </div>
+      <Alert variant="secondary" style={{ paddingBottom: 0 }}>
+        <Form>
+          <Row>
+            <Col>
+              <Form.Group style={{ margin: 0 }}>
+                <Row>
+                  <Col>
+                    <Form.Label>Due Date</Form.Label>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <div>
+                      <DatePicker
+                        id="start"
+                        name="start"
+                        placeholderText="2019-01-20"
+                        className="form-control"
+                        dateFormat="yyyy-MM-dd"
+                        selected={dateStart}
+                        onChange={date => setDateStart(date)}
+                      />
+                    </div>
+                  </Col>
+                  <Col>
+                    <DatePicker
+                      id="end"
+                      name="end"
+                      placeholderText="2019-01-20"
+                      className="form-control"
+                      dateFormat="yyyy-MM-dd"
+                      selected={dateEnd}
+                      onChange={date => setDateEnd(date)}
+                    />
+                  </Col>
+                </Row>
+              </Form.Group>
+            </Col>
+            <Col xs={2}>
+              <Form.Group style={{ margin: 0 }}>
+                <Row>
+                  <Col>&nbsp;</Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Form.Label>
+                      Active Expenses
+                      <Switch
+                        name="active"
+                        id="active"
+                        checked={filterParams.active}
+                        onChange={evt => {
+                          setFilterParams({
+                            ...filterParams,
+                            active: !filterParams.active
+                          });
+                        }}
+                      />
+                    </Form.Label>
+                  </Col>
+                </Row>
+              </Form.Group>
+            </Col>
+            <Col>
+              <Row>
+                <Col>&nbsp;</Col>
+              </Row>
 
-      <div>
-        <label>
-          <input
-            type="checkbox"
-            name="active"
-            id="active"
-            value="1"
-            checked={filterParams.active}
-            onChange={evt => {
-              setFilterParams({
-                ...filterParams,
-                active: !filterParams.active
-              });
-            }}
-          />
-          Active expenses
-        </label>
-      </div>
-
-      <div>
-        <button type="button" onClick={() => onFilter(filterParams)}>
-          Search
-        </button>
-      </div>
+              <Row>
+                <Col>
+                  <Form.Group style={{ margin: 0, marginTop: "10px" }}>
+                    <Button
+                      variant="primary"
+                      type="button"
+                      onClick={() => onFilter(filterParams)}
+                    >
+                      Search
+                    </Button>
+                  </Form.Group>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Form>
+      </Alert>
     </div>
   );
 }
