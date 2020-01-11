80eb1e0ad080bb92bb86492196d8f5bc926978ec
George Bentes <george.bentes@gmail.com>
2020-01-10 21:05:38 -0300

    Refactor form UI

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
+import { Form as FormUI, Button, Container, Row, Col } from "react-bootstrap";
+import DatePicker from "react-datepicker";
+import CurrencyInput from "react-currency-input";
 
 import expenseTypeService from "../services/expenseTypeService";
 import expenseService from "../services/expenseService";
@@ -23,13 +26,17 @@ export default function Form() {
   const [expenseTypes, setExpenseTypes] = useState([]);
   const [expense, setExpense] = useState(newExepense);
   const [periodicities, setPeriodicities] = useState([]);
+  const [formDueDate, setFormDueDate] = useState(new Date());
   const history = useHistory();
 
   React.useEffect(() => {
     expenseTypeService.listAll().then(response => setExpenseTypes(response));
     if (id) {
       expenseService.findById(id).then(
-        response => setExpense(response.data),
+        response => {
+          setExpense(response.data);
+          setFormDueDate(new Date(response.data.dueDate));
+        },
         () => history.push("/notFound")
       );
     } else {
@@ -71,26 +78,30 @@ export default function Form() {
 
   return (
     <div>
-      <form onSubmit={submitForm}>
-        <div>
-          <label htmlFor="title">Description:</label>
-          <input
+      <FormUI onSubmit={submitForm}>
+        <FormUI.Group>
+          <FormUI.Label htmlFor="title">Description</FormUI.Label>
+          <FormUI.Control
             type="text"
             id="title"
             name="title"
             placeholder="Conta de Luz"
+            style={{ width: "50%" }}
+            maxLength={80}
             value={expense.title}
             onChange={evt =>
               setExpense({ ...expense, title: evt.target.value })
             }
           />
-        </div>
+        </FormUI.Group>
 
-        <div>
-          <label htmlFor="type">Type:</label>
-          <select
+        <FormUI.Group>
+          <FormUI.Label htmlFor="type">Type</FormUI.Label>
+          <FormUI.Control
+            as="select"
             id="type"
             name="type"
+            style={{ width: "20%" }}
             value={expense.expenseType ? expense.expenseType.id : 1}
             onChange={evt =>
               setExpense({ ...expense, expenseType: { id: evt.target.value } })
@@ -102,113 +113,71 @@ export default function Form() {
                 {exp.name}
               </option>
             ))}
-          </select>
-        </div>
-        <div>
-          <label htmlFor="cost">Cost:</label>
-          <input
-            type="text"
-            id="cost"
-            name="cost"
-            placeholder="R$ 150,00"
-            value={expense.cost}
-            onChange={evt => setExpense({ ...expense, cost: evt.target.value })}
-          />
-        </div>
-        <div>
-          <label htmlFor="dueDate">Due Date:</label>
-          <input
-            type="date"
-            id="dueDate"
-            name="dueDate"
-            placeholder="2019-01-20"
-            value={expense.dueDate}
-            onChange={evt =>
-              setExpense({ ...expense, dueDate: evt.target.value })
-            }
-          />
-        </div>
+          </FormUI.Control>
+        </FormUI.Group>
 
-        <div>
-          <label style={{ cursor: "pointer" }}>
-            <input type="checkbox" id="repeat" name="repeat" value="1" />
-            Repeat this action
-          </label>
-        </div>
+        <FormUI.Group>
+          <FormUI.Label htmlFor="cost">Cost</FormUI.Label>
+          <div>
+            <CurrencyInput
+              prefix="R$ "
+              decimalSeparator=","
+              thousandSeparator="."
+              id="cost"
+              name="cost"
+              placeholder="R$ 150,00"
+              value={expense.cost}
+              onChange={cost => setExpense({ ...expense, cost })}
+              className="form-control"
+              style={{ width: "20%" }}
+            />
+          </div>
+        </FormUI.Group>
+        <FormUI.Group>
+          <FormUI.Label htmlFor="dueDate">Due Date</FormUI.Label>
+          <div>
+            <DatePicker
+              id="dueDate"
+              name="dueDate"
+              placeholderText="2019-01-20"
+              className="form-control"
+              dateFormat="yyyy-MM-dd"
+              selected={formDueDate}
+              onChange={date => setFormDueDate(date)}
+            />
+          </div>
+        </FormUI.Group>
 
-        <div>
-          <label id="periodicity">Periodicity:</label>
-          <select id="periodicity">
-            {periodicities.map(p => (
+        <FormUI.Group>
+          <FormUI.Check
+            type="checkbox"
+            id="repeat"
+            name="repeat"
+            value="1"
+            label="Repeat this action"
+          />
+        </FormUI.Group>
+
+        <FormUI.Group>
+          <FormUI.Label id="periodicity">Periodicity</FormUI.Label>
+          <FormUI.Control as="select" id="periodicity" style={{ width: "20%" }}>
+            {periodicities.slice(1).map(p => (
               <option key={p.value} value={p.value}>
                 {p.label}
               </option>
             ))}
-          </select>
-        </div>
-
-        <details>
-          <summary>Add more details</summary>
-          <div>
-            <label htmlFor="invoiceDate">Invoice Date:</label>
-            <input
-              type="date"
-              id="invoiceDate"
-              name="invoiceDate"
-              placeholder="2019-01-15"
-              value={expense.invoiceDate}
-              onChange={evt =>
-                setExpense({ ...expense, invoiceDate: evt.target.value })
-              }
-            />
-          </div>
-          <div>
-            <label htmlFor="servicePeriodStart">Service Start:</label>
-            <input
-              type="date"
-              id="servicePeriodStart"
-              name="servicePeriodStart"
-              placeholder="2019-01-01"
-              value={expense.servicePeriodStart}
-              onChange={evt =>
-                setExpense({ ...expense, servicePeriodStart: evt.target.value })
-              }
-            />
-          </div>
-          <div>
-            <label htmlFor="servicePeriodEnd">Service End:</label>
-            <input
-              type="date"
-              id="servicePeriodEnd"
-              name="servicePeriodEnd"
-              placeholder="2019-01-30"
-              value={expense.servicePeriodEnd}
-              onChange={evt =>
-                setExpense({ ...expense, servicePeriodEnd: evt.target.value })
-              }
-            />
-          </div>
-        </details>
-
-        <div>
-          <label htmlFor="description">Comments:</label>
-          <textarea
-            type="text"
-            id="description"
-            name="description"
-            value={expense.description}
-            onChange={evt =>
-              setExpense({ ...expense, description: evt.target.value })
-            }
-          ></textarea>
-        </div>
+          </FormUI.Control>
+        </FormUI.Group>
 
         {expense.paid && (
-          <div>
-            <label htmlFor="paid">Status: </label>
-            <select
+          <FormUI.Group>
+            <FormUI.Label htmlFor="paid">Status</FormUI.Label>
+            <FormUI.Control
+              as="select"
               name="paid"
               id="paid"
+              className="form-control"
+              style={{ width: "50%" }}
               value={expense.paid}
               onChange={evt =>
                 setExpense({ ...expense, paid: evt.target.value })
@@ -216,24 +185,55 @@ export default function Form() {
             >
               <option value={false}>Not Paid</option>
               <option value={true}>Paid</option>
-            </select>
-          </div>
+            </FormUI.Control>
+          </FormUI.Group>
         )}
 
-        <div>
-          <button type="button" onClick={() => cancel()}>
-            Cancel
-          </button>
+        <FormUI.Group>
+          <FormUI.Label htmlFor="description">Comments</FormUI.Label>
+          <FormUI.Text
+            type="text"
+            id="description"
+            name="description"
+            as="textarea"
+            className="form-control"
+            rows="3"
+            style={{ width: "50%" }}
+            value={expense.description}
+            onChange={evt =>
+              setExpense({ ...expense, description: evt.target.value })
+            }
+          />
+        </FormUI.Group>
 
-          {id && (
-            <button type="button" onClick={() => disableExpense()}>
-              Delete
-            </button>
-          )}
+        <FormUI.Group style={{ width: "65%" }}>
+          <Container>
+            <Row>
+              <Col>
+                <Button type="button" variant="light" onClick={() => cancel()}>
+                  Cancel
+                </Button>
+              </Col>
 
-          <button type="submit">Save</button>
-        </div>
-      </form>
+              <Col>
+                {id && (
+                  <Button
+                    type="button"
+                    variant="danger"
+                    onClick={() => disableExpense()}
+                  >
+                    Delete
+                  </Button>
+                )}
+              </Col>
+
+              <Col>
+                <Button type="submit">Save</Button>
+              </Col>
+            </Row>
+          </Container>
+        </FormUI.Group>
+      </FormUI>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-datepicker/dist/react-datepicker.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
