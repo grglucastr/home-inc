5df94e510b532a3d3d406dd0175f25d8138a659f
George Bentes <george.bentes@gmail.com>
2020-01-11 10:06:29 -0300

    Refactor Table UI

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,18 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Table as TableUI } from "react-bootstrap";
 
 export default function Table({ expenses, onPay }) {
-  if (!expenses || expenses.length === 0) {
-    return (
-      <div>
-        <p>Empty values</p>
-      </div>
-    );
-  }
-
   return (
     <div>
-      <table>
+      <TableUI striped size="sm">
         <thead>
           <tr>
             <th>Expense</th>
@@ -23,25 +16,37 @@ export default function Table({ expenses, onPay }) {
           </tr>
         </thead>
 
-        <tbody>
-          {expenses.map(expense => (
-            <tr key={expense.id}>
-              <td>{expense.title}</td>
-              <td>{expense.cost}</td>
-              <td>{expense.dueDate}</td>
-              <td>
-                {expense.paid && <span>Paid.</span>}
-                {!expense.paid && (
-                  <button onClick={() => onPay(expense)}>Pay</button>
-                )}
-              </td>
-              <td>
-                <Link to={`/expense/${expense.id}`}>Details</Link>
+        {(expenses || expenses.length > 0) && (
+          <tbody>
+            {expenses.map(expense => (
+              <tr key={expense.id}>
+                <td>{expense.title}</td>
+                <td>{expense.cost}</td>
+                <td>{expense.dueDate}</td>
+                <td>
+                  {expense.paid && <span>Paid.</span>}
+                  {!expense.paid && (
+                    <button onClick={() => onPay(expense)}>Pay</button>
+                  )}
+                </td>
+                <td>
+                  <Link to={`/expense/${expense.id}`}>Details</Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        )}
+
+        {(!expenses || expenses.length === 0) && (
+          <tbody>
+            <tr>
+              <td colspan="5" align="center">
+                Empty Values
               </td>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </tbody>
+        )}
+      </TableUI>
     </div>
   );
 }
