45c8204f3f21e7cb45126a1438dfd86f6ebf0094
George Bentes <george.bentes@gmail.com>
2020-01-15 07:33:29 -0300

    Add currency formatting

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Table as TableUI } from "react-bootstrap";
+import currency from "currency.js";
 
 export default function Table({ expenses, onPay }) {
+  const BRL = value =>
+    currency(value, {
+      symbol: "R$ ",
+      separator: ".",
+      decimal: ",",
+      precision: 2
+    });
+
   return (
     <div>
       <TableUI striped size="sm">
@@ -21,7 +30,7 @@ export default function Table({ expenses, onPay }) {
             {expenses.map(expense => (
               <tr key={expense.id}>
                 <td>{expense.title}</td>
-                <td>{expense.cost}</td>
+                <td>{BRL(expense.cost).format(true)}</td>
                 <td>{expense.dueDate}</td>
                 <td>
                   {expense.paid && <span>Paid.</span>}
